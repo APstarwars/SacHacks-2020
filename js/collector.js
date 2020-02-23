3f3d2b9aff166daee68b6a3a3471d42b7c6f6974
APstarwars <32665017+APstarwars@users.noreply.github.com>
2020-02-23 03:04:33 -0800

    Add Stats
    Added Player stats in the window

--- js/collector.js
+++ js/collector.js
@@ -17,6 +17,9 @@ function playerParser() {
       var myObj = JSON.parse(this.responseText);
       for (let x = 0; x < myObj.length; x++) {
         document.getElementsByClassName("players")[x].innerHTML = myObj[x].Player;
+        document.getElementsByClassName("FGM")[x].innerHTML = myObj[x].FGM;
+        document.getElementsByClassName("PTS")[x].innerHTML = myObj[x].PTS;
+        document.getElementsByClassName("AST")[x].innerHTML = myObj[x].AST;
       }
     };
     xmlhttp.open("GET", "data/All_Players_Ranking.txt", true);
