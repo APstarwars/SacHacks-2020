e221b4e7adbde36965c00f0d2c42c9c95b727b43
Linda <lindathehuang@gmail.com>
2020-02-23 09:36:03 -0800

    Added Weakness Detector
    W E A K
    jk lmao we done fam

--- js/collector.js
+++ js/collector.js
@@ -4,10 +4,19 @@ function teamsParser() {
     xmlhttp.onload = function() {
       var myObj = JSON.parse(this.responseText);
       for (let x = 0; x < myObj.length; x++) {
-        document.getElementsByClassName("teams")[x].innerHTML = (x+1) + ". " + myObj[x].Team;
-        document.getElementsByClassName("FGM")[x].innerHTML = (x+1) + ". " + myObj[x].FGP;
-        document.getElementsByClassName("PTS")[x].innerHTML = (x+1) + ". " + myObj[x].Points;
-        document.getElementsByClassName("AST")[x].innerHTML = (x+1) + ". " + myObj[x].Assists;
+        document.getElementsByClassName("teams")[x].innerHTML = myObj[x].Team;
+        document.getElementsByClassName("FGM")[x].innerHTML = myObj[x].FGP;
+        if(myObj[x].FGP == "FGP: Weaknesses"){
+          document.getElementsByClassName("FGM")[x].classList.add("weak");
+        }
+        document.getElementsByClassName("PTS")[x].innerHTML = myObj[x].Points;
+        if(myObj[x].Points == "Points: Weaknesses"){
+          document.getElementsByClassName("PTS")[x].classList.add("weak");
+        }
+        document.getElementsByClassName("AST")[x].innerHTML = myObj[x].Assists;
+        if(myObj[x].Assists == "Assists: Weaknesses"){
+          document.getElementsByClassName("AST")[x].classList.add("weak");
+        }
       }
     };
     xmlhttp.open("GET", "data/Team_Weak_Str.txt", true);
@@ -19,11 +28,20 @@ function playerParser() {
     xmlhttp.onload = function() {
       var myObj = JSON.parse(this.responseText);
       for (let x = 0; x < myObj.length; x++) {
-        document.getElementsByClassName("players")[x].innerHTML = (x+1) + ". " + myObj[x].Player;
+        document.getElementsByClassName("players")[x].innerHTML = myObj[x].Player;
         document.getElementsByClassName("players")[x].setAttribute("id", myObj[x].Team);
         document.getElementsByClassName("FGM")[x].innerHTML = myObj[x].FGM;
+        if(myObj[x].FGM <= 50){
+          document.getElementsByClassName("FGM")[x].classList.add("weak");
+        }
         document.getElementsByClassName("PTS")[x].innerHTML = myObj[x].PTS;
+        if(myObj[x].PTS <= 100){
+          document.getElementsByClassName("PTS")[x].classList.add("weak");
+        }
         document.getElementsByClassName("AST")[x].innerHTML = myObj[x].AST;
+        if(myObj[x].AST <= 30){
+          document.getElementsByClassName("AST")[x].classList.add("weak");
+        }
       }
     };
     xmlhttp.open("GET", "data/All_Players_Ranking.txt", true);
