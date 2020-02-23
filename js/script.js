a9428a818b2837e63f850de5d445f84a17d37314
Linda <lindathehuang@gmail.com>
2020-02-22 19:06:48 -0800

    Adjusted HTML and added some JS
    Reformatted and added JS for interaction

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-//
+$(document).ready(function(){
+  $(".statsec h2").click(function(){
+    $(".statsec h2").css({"background":"#0A141A", "color":"rgba(255, 255, 255, 0.5)"});
+    $(this).css({"background":"#0b1013", "color":"rgba(255, 255, 255, 1)"});
+  });
+});
+
+window.onload = function() {
+  document.getElementById()
+};
